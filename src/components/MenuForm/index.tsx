d679df27c31f783f16a27c5a38dc8601951dc17c
Maicon Andrade <andrade.maicon.pr@gmail.com>
2021-02-28 20:35:35 -0300

    Recreated Dropbox interface with React JS

---
+++ src/components/MenuForm/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { Container, Navigation, DropboxLogo, Form } from './styles';
+
+const MenuForm: React.FC = () => {
+  function handleToggle() {
+    if (window.toggleActiveMenu) window.toggleActiveMenu();
+  }
+
+   return (
+    <Container>
+      <Navigation>
+        <h1>
+          <DropboxLogo />
+          <span>Dropbox</span>
+        </h1>
+
+        <button className="action--close" onClick={handleToggle}>✕</button>
+      </Navigation>
+
+      <Form>
+        <span className="title">Registre-se</span>
+        <span className="subtitle">preencha o formulario abaixo</span>
+
+        <input type="text" placeholder="Nome"/>
+        <input type="text" placeholder="Sobrenome"/>
+        <input type="email" placeholder="E-mail"/>
+        <input type="password" placeholder="Senha"/>
+
+        <button>Prosseguir</button>
+
+        <span className="terms">
+          Esta pagina esta sujeita a Politica de privacidade i aos Termos de servico.
+        </span>
+      </Form>
+    </Container>
+  );
+};
+
+export default MenuForm;
